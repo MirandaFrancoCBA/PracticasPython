80ac5f54246f1a92331574025775c56921e1bc9a
MirandaFrancoCBA <167719225+MirandaFrancoCBA@users.noreply.github.com>
2024-07-23 01:47:53 +0000

    cuidado con indentacion

--- herenciaMultiple.py
+++ herenciaMultiple.py
@@ -1,11 +1,47 @@
 class FiguraGeometrica:
     def __init__(self, ancho, alto):
-        self.ancho = ancho
-        self.alto = alto
+        self._ancho = ancho
+        self._alto = alto
+
+    @property
+
+    def ancho(self):
+        return self._ancho
+
+    @ancho.setter
+
+    def ancho(self, ancho):
+        self._ancho = ancho
+
+    @property
+
+    def alto(self):
+        return self._alto
+    @alto.setter
+
+    def alto(self, alto):
+        self._alto = alto
+
+    def __str__(self):
+        return f"Figura Geometrica [Ancho: {self._ancho} Alto: {self._alto} ]"
+
 
 class Color:
     def __init__(self, color):
-        self.color = color
+        self._color = color
+
+    @property
+
+    def color(self):
+        return self._color
+
+    @color.setter 
+
+    def color(self, color):
+        self._color = color
+
+    def __str__(self):
+        return f"Color: {self._color}"
 
 class Cuadrado(FiguraGeometrica, Color):
     def __init__(self, lado, color):
@@ -13,10 +49,13 @@ class Cuadrado(FiguraGeometrica, Color):
         Color.__init__(self, color)
     
     def calcular_area(self):
-        return self.ancho * self.alto
+        return self._alto * self._ancho
+    
+    def __str__(self):
+        return f"{FiguraGeometrica.__str__(self)} {Color.__str__(self)}"
+
     
 cuadrado1 = Cuadrado(4, "Rojo")
+print(cuadrado1)
 print(cuadrado1.calcular_area())
-print(cuadrado1.color)
-
     
